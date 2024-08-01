d6b00893d80df5b214d888efb25005dc490cfef8
Jay <49499730+PureGravity@users.noreply.github.com>
2024-08-01 19:53:30 -0400

    Fix: updated minor syntax issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let square = document.querySelector('.square')
-let container = document.querySelector('.container')
+let square = document.querySelector('.square');
+let container = document.querySelector('.container');
 
 let y = 5;
 let x = 32;
@@ -14,7 +14,7 @@ for (let i = 0; i < y; i++) {
     container.appendChild(newRow);
 }
 
-let allSquares = document.querySelectorAll('.square')
+let allSquares = document.querySelectorAll('.square');
 
 allSquares.forEach(function(shape){
     shape.addEventListener('mouseover', function(){
@@ -25,7 +25,7 @@ allSquares.forEach(function(shape){
 
 // clear button
 
-let clearBtn = document.querySelector('.clearButton')
+let clearBtn = document.querySelector('.clearButton');
 
 allSquares.forEach(function(shape){
     clearBtn.addEventListener('click', function(){
@@ -35,19 +35,19 @@ allSquares.forEach(function(shape){
 })
 
 clearBtn.addEventListener('mouseover', function(){
-    clearBtn.style.outline = '3px solid white'
+    clearBtn.style.outline = '3px solid white';
 
 })
 clearBtn.addEventListener('mouseout', function(){
-    clearBtn.style.outline = ''
+    clearBtn.style.outline = '';
 })
 
 // color buttons
 
-let red = document.querySelector('.redColor')
-let black = document.querySelector('.blackColor')
-let green = document.querySelector('.greenColor')
-let blue = document.querySelector('.blueColor')
+let red = document.querySelector('.redColor');
+let black = document.querySelector('.blackColor');
+let green = document.querySelector('.greenColor');
+let blue = document.querySelector('.blueColor');
 
 red.addEventListener('click', function(){
     allSquares.forEach(function(shape){
@@ -82,12 +82,12 @@ blue.addEventListener('click', function(){
 })
 
 // Github Link 
- link = document.querySelector('.ghLink')
+ link = document.querySelector('.ghLink');
 
 link.addEventListener('mouseover', function(){
-    link.style.color = 'white'
+    link.style.color = 'white';
 
 })
 link.addEventListener('mouseout', function(){
-    link.style.color = ''
+    link.style.color = '';
 })
