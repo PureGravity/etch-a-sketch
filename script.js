fbf714d934e5e4a5f0ccd63e826dd6751a69c35c
Jay <49499730+PureGravity@users.noreply.github.com>
2024-07-31 21:13:46 -0400

    Feat: add functionality for color picker

--- script.js
+++ script.js
@@ -20,17 +20,74 @@ allSquares.forEach(function(shape){
     shape.addEventListener('mouseover', function(){
         shape.style.backgroundColor = 'black';
     });
-
-})
+});
 
 
 // clear button
 
-let clearBtn = document.querySelector('.clear')
+let clearBtn = document.querySelector('.clearButton')
 
 allSquares.forEach(function(shape){
     clearBtn.addEventListener('click', function(){
         shape.style.backgroundColor = '';
     });
 
+})
+
+clearBtn.addEventListener('mouseover', function(){
+    clearBtn.style.outline = '3px solid white'
+
+})
+clearBtn.addEventListener('mouseout', function(){
+    clearBtn.style.outline = ''
+})
+
+// color buttons
+
+let red = document.querySelector('.redColor')
+let black = document.querySelector('.blackColor')
+let green = document.querySelector('.greenColor')
+let blue = document.querySelector('.blueColor')
+
+red.addEventListener('click', function(){
+    allSquares.forEach(function(shape){
+        shape.addEventListener('mouseover', function(){
+            shape.style.backgroundColor = 'red';
+        });
+    });
+})
+
+black.addEventListener('click', function(){
+    allSquares.forEach(function(shape){
+        shape.addEventListener('mouseover', function(){
+            shape.style.backgroundColor = 'black';
+        });
+    });
+})
+
+green.addEventListener('click', function(){
+    allSquares.forEach(function(shape){
+        shape.addEventListener('mouseover', function(){
+            shape.style.backgroundColor = 'green';
+        });
+    });
+})
+
+blue.addEventListener('click', function(){
+    allSquares.forEach(function(shape){
+        shape.addEventListener('mouseover', function(){
+            shape.style.backgroundColor = 'blue';
+        });
+    });
+})
+
+// Github Link 
+ link = document.querySelector('.ghLink')
+
+link.addEventListener('mouseover', function(){
+    link.style.color = 'white'
+
+})
+link.addEventListener('mouseout', function(){
+    link.style.color = ''
 })
